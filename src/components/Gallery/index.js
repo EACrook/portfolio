dc330cdba9d29a8fa1e6d599030accc20151ae72
Emily Crook <Emily.A.Crook96@gmail.com>
2021-08-31 20:53:54 -0500

    start of project gallery page

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Gallery(props) {
+    const currentProject = {
+        name: "test",
+        description: "this is a test"
+    };
+    return (
+        <section>
+            <h1></h1>
+            <p>DESCRIPTION</p>
+        </section>
+    );
+}
+
+export default Gallery;
